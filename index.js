76b5b60ad6a63e38f336c68bf3ec3200c3347cfa
KyleNBledsoe <kylenbledsoe@gmail.com>
2026-02-28 20:56:50 -0600

    Added make blue functionality.

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function makeGreen() {
 	console.log("Calling makeGreen function")
 	document.getElementById("msg").classList.add("green");
 }
+
+function makeBlue() {
+	console.log("Calling makeBlue function")
+	document.getElementById("msg").classList.add("blue");
+}
